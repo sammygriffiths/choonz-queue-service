fb69327265ccefc35eac8ff54640ecec88678b5f
Sammy Griffiths <samuel.griffiths@skybettingandgaming.com>
2017-09-14 09:32:13 +0100

    Add tests for sending spotify id

--- src/lib/api/routes/queue.js
+++ src/lib/api/routes/queue.js
@@ -2,15 +2,17 @@
 
 const joi = require('joi');
 
-let queue = {};
+let queue = {
+    song: {}
+};
 
-queue.add = {
+queue.song.add = {
     handler: (req, res, next) => {
         res.send('string');
     },
     clientInputSchema: joi.object().keys({
         body: joi.object().keys({
-            
+            spotify_id: joi.string().alphanum().min(22).max(22)
         }).required()
     }).required()
 };
--- test/js/api/queue.test.js
+++ test/js/api/queue.test.js
@@ -2,19 +2,44 @@
 
 const expect = require('chai').expect;
 const sinon = require('sinon');
-const os = require('os');
 
 const queue = require('../../../src/lib/api/routes/queue.js');
 
-describe('Add route', () => {
-    it('doesn\'t validate when no body is given', () => {
-        const result = queue.add.clientInputSchema.validate();
+describe('Song route', () => {
+    describe('Add', () => {
+        it('doesn\'t validate when no object is given', () => {
+            const result = queue.song.add.clientInputSchema.validate();
 
-        expect(result.error).to.not.equal(null);
-    });
-    it('doesn\'t validate when an empty body is given', () => {
-        const result = queue.add.clientInputSchema.validate({});
+            expect(result.error).to.not.equal(null);
+        });
+        it('doesn\'t validate when an empty object is given', () => {
+            const result = queue.song.add.clientInputSchema.validate({});
 
-        expect(result.error).to.not.equal(null);
+            expect(result.error).to.not.equal(null);
+        });
+
+        describe('Spotify', () => {
+            it('accepts a spotify_id param', () => {
+                let req = {
+                    body: {
+                        spotify_id: '6rqhFgbbKwnb9MLmUQDhG6'
+                    }
+                };
+                const result = queue.song.add.clientInputSchema.validate(req);
+                
+                expect(result.error).to.equal(null);
+            });
+
+            it('doesn\'t validate when an invalid song id is given', () => {
+                let req = {
+                    body: {
+                        spotify_id: 'invalid'
+                    }
+                };
+                const result = queue.song.add.clientInputSchema.validate(req);
+                
+                expect(result.error).to.not.equal(null);
+            });
+        })
     });
 });
