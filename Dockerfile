cd64048f2747c87f83af60902eedddfc735e68f1
Sammy Griffiths <samuel.griffiths@skybettingandgaming.com>
2017-10-21 00:03:39 +0100

    Remove kitchen and replace it with a simple dockerfile

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM node:8
+
+WORKDIR /code
+ENTRYPOINT node src/bin/www
+
+COPY package.json package-lock.json /code/
+RUN npm install
+
+COPY ./ /code/
--- recipes/zookeeper.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook:: choonz-queue-service
-# Recipe:: zookeeper
-#
-# Copyright:: 2017, The Authors, All Rights Reserved.
-
-node.default['zookeeper']['service_style'] = 'upstart'
-include_recipe 'zookeeper'
-include_recipe 'zookeeper::service'
