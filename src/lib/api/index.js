7dac0a5fb3ff9692470a82766f48e47ee38c2692
Sammy Griffiths <samuel.griffiths@skybettingandgaming.com>
2017-10-23 20:45:15 +0100

    Add validation to route

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -3,6 +3,11 @@
 const router = require('express').Router();
 const queue = require('./routes/queue');
 
-router.get('/song/add', queue.song.add.handler);
+const validateClientInput = require('../middleware/validateClientInput');
+
+router.post('/song/add',
+    validateClientInput(queue.song.add.clientInputSchema),
+    queue.song.add.handler
+);
 
 module.exports = router;
