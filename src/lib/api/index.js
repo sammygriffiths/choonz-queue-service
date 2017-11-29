9aa8b3dbc7179e1c2ec67850ce285c9524b4dcf5
Sammy Griffiths <samuel.griffiths@skybettingandgaming.com>
2017-11-29 18:03:57 +0000

    Rework dependencies

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -1,16 +1,17 @@
 'use strict';
 
-const Sonos = require('sonos').Sonos;
 const router = require('express').Router();
 const queue = require('./routes/queue');
 
 const validateClientInput = require('../middleware/validateClientInput');
 
-const sonos = new Sonos(process.env.SONOS_HOST || '192.168.1.8');
+module.exports = (dependencies) => {
+    const { sonos, redis } = dependencies;
 
-router.post('/song/add',
-    validateClientInput(queue.song.add.clientInputSchema),
-    queue.song.add.handler(sonos)
-);
+    router.post('/song/add',
+        validateClientInput(queue.song.add.clientInputSchema),
+        queue.song.add.handler(sonos)
+    );
 
-module.exports = router;
+    return router;
+};
