8eddfe27858f3ed0f435e56db9cda93866166533
Sammy Griffiths <samuel.griffiths@skybettingandgaming.com>
2018-06-27 17:52:43 +0100

    Add environment variable for react app url

--- src/app.js
+++ src/app.js
@@ -27,7 +27,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', process.env.REACT_APP_URL);
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 
 app.use('/', api({ sonos, redis }));
 
