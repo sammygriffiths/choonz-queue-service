74bda83e7d03daf580b5b592d9b48274cf082ee5
Sammy Griffiths <samuel.griffiths@skybettingandgaming.com>
2018-06-29 17:51:54 +0100

    Allow appropriate methods in CORS

--- src/lib/api/index.js
+++ src/lib/api/index.js
@@ -8,13 +8,20 @@ const validateClientInput = require('../middleware/validateClientInput');
 module.exports = (dependencies) => {
     const { sonos, redis } = dependencies;
 
+    router.options('/songs', (req, res, next) => {
+        res.append('Access-Control-Allow-Methods', 'POST, DELETE');
+        next();
+    });
     router.post('/songs',
         validateClientInput(queue.songs.create.clientInputSchema),
         queue.songs.create.handler(sonos, redis)
     );
-
     router.delete('/songs', queue.songs.delete.handler(sonos, redis));
 
+    router.options('/songs/recently-added', (req, res, next) => {
+        res.append('Access-Control-Allow-Methods', 'DELETE');
+        next();
+    });
     router.delete('/songs/recently-added', queue.songs.recentlyAdded.delete.handler(redis));
 
     return router;
